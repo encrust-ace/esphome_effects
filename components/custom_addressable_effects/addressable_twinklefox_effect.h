b8900bc8639fe9f009673bce415510f95954b902
Imran Khan <encrustace@gmail.com>
2026-01-04 03:37:35 +0530

    Dynamic pelette working

--- components/custom_addressable_effects/addressable_twinklefox_effect.h
+++ components/custom_addressable_effects/addressable_twinklefox_effect.h
@@ -84,7 +84,7 @@ class AddressableTwinkleFoxEffect : public AddressableLightEffect {
   void set_palette(TwinkleFoxPaletteType palette) { this->palette_type_ = palette; }
   void set_palette(const char *palette) {
     std::string p(palette);
-    ESP_LOGD("set_palette received: %s", p);
+    ESP_LOGD("set_palette received: %s", p.c_str());
     if (p == "party_colors") this->palette_type_ = PALETTE_PARTY_COLORS;
     else if (p == "ocean_colors") this->palette_type_ = PALETTE_OCEAN_COLORS;
     else if (p == "lava_colors") this->palette_type_ = PALETTE_LAVA_COLORS;
